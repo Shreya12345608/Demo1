e010191917c0abc476b6a2f49e10ca73e3996581
Shreya <malviyashreya26@gmail.com>
2021-11-03 20:01:31 +0530

    [Shreya]Add: : Check EMp is present or absent

--- EmpWageCom/EmpWageCom/Program.cs
+++ EmpWageCom/EmpWageCom/Program.cs
@@ -7,6 +7,10 @@ namespace EmpWageCom
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee Wage Program"); //main
+            EmployeWageProblem EmployeeWage = new EmployeWageProblem();
+            EmployeeWage.Attendance();
+            Console.ReadKey();
         }
     }
 }
